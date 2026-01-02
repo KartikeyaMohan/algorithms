14de1e996acc4d64c8a8b4ec8dc49b11b86c6ef1
Kartikeya <kartikeyamohan@gmail.com>
2026-01-02 20:14:23 +0530

    chore: [graphs] Refactoring

--- graphs/BreadthFirstSearch.java
+++ graphs/BreadthFirstSearch.java
@@ -8,19 +8,18 @@ class BreadthFirstSearch {
         boolean[] visited = new boolean[graph.length];
         int index = 0;
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(index);
-        visited[index] = true;
+        queue.add(0);
+        visited[0] = true;
         while(index < graph.length) {
             if (!queue.isEmpty()) {
                 int node = queue.poll();
-                traversed[index] = node;
-                for (int i = 0; i < graph[index].length; i++) {
-                    if (!visited[graph[index][i]]) {
-                        visited[graph[index][i]] = true;
-                        queue.add(graph[index][i]);
+                traversed[index++] = node;
+                for (int i = 0; i < graph[node].length; i++) {
+                    if (!visited[graph[node][i]]) {
+                        visited[graph[node][i]] = true;
+                        queue.add(graph[node][i]);
                     }
                 }
-                index++;
             }
             else {
                 queue.add(index);
@@ -32,10 +31,18 @@ class BreadthFirstSearch {
 
     public static void main(String[] args) {
         BreadthFirstSearch object = new BreadthFirstSearch();
-        int[][] graph = {{2, 3}, {2}, {0, 1}, {0}, {5}, {4}};
-        int[] output = object.traversal(graph);
-        for (int i = 0; i < output.length; i++) {
-            System.out.print(output[i] + (i == output.length - 1 ? "" : ", "));
+        int[][] connectedGraph = {{1, 2}, {0, 2}, {0, 1, 3, 4}, {2}, {2}};
+        int[][] disconnectedGraph = {{2, 3}, {2}, {0, 1}, {0}, {5}, {4}};
+        int[] connectedGraphOutput = object.traversal(connectedGraph);
+        int[] disconnectedGraphOutput = object.traversal(disconnectedGraph);
+        System.out.print("Connected Graph output: ");
+        for (int i = 0; i < connectedGraphOutput.length; i++) {
+            System.out.print(connectedGraphOutput[i] + (i == connectedGraphOutput.length - 1 ? "" : ", "));
+        }
+        System.out.println();
+        System.out.print("Disconnected Graph output: ");
+        for (int i = 0; i < disconnectedGraphOutput.length; i++) {
+            System.out.print(disconnectedGraphOutput[i] + (i == disconnectedGraphOutput.length - 1 ? "" : ", "));
         }
     }
 }
--- graphs/DepthFirstSearch.java
+++ graphs/DepthFirstSearch.java
@@ -7,34 +7,41 @@ class DepthFirstSearch {
         boolean[] visited = new boolean[graph.length];
         int index = 0;
         Stack<Integer> stack = new Stack<>();
-        stack.add(index);
-        visited[index] = true;
-        while (index < graph.length) {
+        stack.add(0);
+        visited[0] = true;
+        while(index < graph.length) {
             if (!stack.isEmpty()) {
                 int node = stack.pop();
-                traversed[index] = node;
-                for(int i = 0; i < graph[index].length; i++) {
-                    if (!visited[graph[index][i]]) {
-                        visited[graph[index][i]] = true;
-                        stack.add(graph[index][i]);
+                traversed[index++] = node;
+                for(int i = 0; i < graph[node].length; i++) {
+                    if (!visited[graph[node][i]]) {
+                        visited[graph[node][i]] = true;
+                        stack.add(graph[node][i]);
                     }
                 }
-                index++;
             }
             else {
                 stack.add(index);
                 visited[index] = true;
             }
         }
-        return traversed;   
+        return traversed;
     }
 
     public static void main(String[] args) {
         DepthFirstSearch object = new DepthFirstSearch();
-        int[][] graph = {{2, 3}, {2}, {0, 1}, {0}, {5}, {4}};
-        int[] output = object.traversal(graph);
-        for (int i = 0; i < output.length; i++) {
-            System.out.print(output[i] + (i == output.length - 1 ? "" : ", "));
+        int[][] connectedGraph = {{1, 2}, {0, 2}, {0, 1, 3, 4}, {2}, {2}};
+        int[][] disconnectedGraph = {{2, 3}, {2}, {0, 1}, {0}, {5}, {4}};
+        int[] connectedGraphOutput = object.traversal(connectedGraph);
+        int[] disconnectedGraphOutput = object.traversal(disconnectedGraph);
+        System.out.print("Connected Graph output: ");
+        for (int i = 0; i < connectedGraphOutput.length; i++) {
+            System.out.print(connectedGraphOutput[i] + (i == connectedGraphOutput.length - 1 ? "" : ", "));
+        }
+        System.out.println();
+        System.out.print("Disconnected Graph output: ");
+        for (int i = 0; i < disconnectedGraphOutput.length; i++) {
+            System.out.print(disconnectedGraphOutput[i] + (i == disconnectedGraphOutput.length - 1 ? "" : ", "));
         }
     }
 }
